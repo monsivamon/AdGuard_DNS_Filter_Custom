2171a28f29bcee70d9726b386ec4696becc15a4c
monsivamon <brackdocomo@outlook.com>
2025-04-20 23:33:58 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@
 set -e
 
 # Compiling AdGuard DNS filter
-hostlist-compiler -c configuration.json -o Filters/filter.txt --verbose
+hostlist-compiler -c configuration.json -o Filters/main_filter.txt --verbose
 
 # Compiling AdGuard DNS Popup Hosts filter
-hostlist-compiler -c configuration_popup_filter.json -o Filters/adguard_popup_filter.txt --verbose
-node scripts/popup_filter_build.js Filters/adguard_popup_filter.txt
+hostlist-compiler -c configuration_popup_filter.json -o Filters/popup_filter.txt --verbose
+node scripts/popup_filter_build.js Filters/popup_filter.txt
