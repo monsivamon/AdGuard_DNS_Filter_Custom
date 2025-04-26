980517b793921d6d927c1322d41fbc04d68747ff
monsivamon <brackdocomo@outlook.com>
2025-04-26 20:24:41 +0900

    Update build.sh
    Signed-off-by: monsivamon <brackdocomo@outlook.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,5 @@ set -e
 hostlist-compiler -c configuration.json -o Filters/main_filter.txt --verbose
 
 # Compiling AdGuard DNS Popup Hosts filter
-hostlist-compiler -c configuration_popup_filter.json -o Filters/popup_filter.txt --verbose
-node scripts/popup_filter_build.js Filters/popup_filter.txt
+# hostlist-compiler -c configuration_popup_filter.json -o Filters/popup_filter.txt --verbose
+# node scripts/popup_filter_build.js Filters/popup_filter.txt
